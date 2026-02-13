707a7a8880e7ed7b909067090fa3b2f1d5cc568b
s4026172 <sandhyaesther4317@gmail.com>
2026-02-13 13:30:44 +0300

    updated packages to their latest versions

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('voltage_it_labs/', admin.site.urls),
-    path('api/', include('djoser.urls')),
-    path('api/', include('accounts.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('accounts.urls')),
 ]
